e83c6ce2591b85c2376a321ac1810d39afff5716
hoolev <hoolev@163.com>
2015-03-25 09:51:23 +0800

    add device start time

--- client/iot.js
+++ client/iot.js
@@ -18,16 +18,16 @@ Template.iotDevice.rendered = function() {
   device = judevs.findOne({'devid':devid});
   console.log('device:',device);
 
-  if(device.status === 'stop') 
+  if(device.status === 'off') 
     return;
 
   now = new Date().getTime();
-  delta = d3.ratio.clip2bars(now - (new Date(device.start).getTime()), 0, 86400 * 1000);
+  delta = d3.ratio.clip2bars(now - (new Date(device.startTime).getTime()), 0, 86400 * 1000);
   // running time
   arcs.push({ 
     name : 'start', 
     raw : device.start, 
-    label : 'TIME', 
+    label : '运行时间', 
     cooked : d3.timestamp.ago(device.start), 
     ratio : delta, 
     index : 0.70
--- server/daemon.js
+++ server/daemon.js
@@ -1,9 +1,11 @@
 Meteor.startup(function() {
+  var now = new Date().getTime(); 
+
   judevs.remove({});
-  judevs.insert({ "devid" : "P28nMQedRNHmSiPAN", "location" : "home.parlour", "type" : "lighting", "connector" : "alljoyn", "name" : "客厅", "status" : "running", "statusColor" : "#008000", "icon" : "lighting-bulb.svg" });
-  judevs.insert({"devid" : "P28nMQedRNHmSiPAA", "location" : "home.parlour", "type" : "media", "connector" : "alljoyn", "name" : "电视", "status" : "running", "statusColor" : "#008000", "icon" : "media-video.svg"});
-  judevs.insert({"devid" : "P28nMQedRNHmSiPAB", "location" : "home.parlour", "type" : "sensor", "connector" : "alljoyn", "name" : "门铃", "status" : "running", "statusColor" : "#008000", "icon" : "sensor-door.svg"});
-  judevs.insert({"devid" : "P28nMQedRNHmSiPAC", "location" : "home.parlour", "type" : "switch", "connector" : "alljoyn", "name" : "插座", "status" : "stop", "statusColor" : "#cccccc", "icon" : "switch-onoff.svg"});
+  judevs.insert({ "devid" : "P28nMQedRNHmSiPAN", "location" : "home.parlour", "type" : "lighting", "connector" : "alljoyn", "name" : "客厅", "status" : "on", "startTime": now, "statusColor" : "#008000", "icon" : "lighting-bulb.svg" });
+  judevs.insert({"devid" : "P28nMQedRNHmSiPAA", "location" : "home.parlour", "type" : "media", "connector" : "alljoyn", "name" : "电视", "status" : "on", "startTime": now, "statusColor" : "#008000", "icon" : "media-video.svg"});
+  judevs.insert({"devid" : "P28nMQedRNHmSiPAB", "location" : "home.parlour", "type" : "sensor", "connector" : "alljoyn", "name" : "门铃", "status" : "on", "startTime": now, "statusColor" : "#008000", "icon" : "sensor-door.svg"});
+  judevs.insert({"devid" : "P28nMQedRNHmSiPAC", "location" : "home.parlour", "type" : "switch", "connector" : "alljoyn", "name" : "插座", "status" : "off", "startTime": "doom","statusColor" : "#cccccc", "icon" : "switch-onoff.svg"});
 
   juhome.remove({});
   juhome.insert({ "authorized" : [  "ntftest" ], "devid" : "P28nMQedRNHmSiPAN", "location" : "home.parlour", "method" : "adjustBrightness", "property" : "brightness", "service" : "lighting", "value" : "100", "label" : "亮度"});
