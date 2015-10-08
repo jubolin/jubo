219e344b8f617cd86da82bb9630e23ba46c4ce1d
hoolev <hoolev@163.com>
2015-10-08 18:07:02 +0800

    refactor file structure

---
+++ packages/jubo-zigbee/package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: "jubo:zigbee",
+  summary: "Jubo zigbee package",
+  version: "0.1.0",
+});
+
+Package.onUse(function(api) {
+});
+
+Npm.depends({
+  serialport: "1.6.1"
+});
+
+
